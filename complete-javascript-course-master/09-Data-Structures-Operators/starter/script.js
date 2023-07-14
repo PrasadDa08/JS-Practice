82d6d1ab7a635e2082589e3e6d8a5ed9b7d11b40
PrasadDa08 <prasaddatir05@gmail.com>
2023-07-14 05:39:26 +0530

    Updated

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
+
+// const arr = [2, 3, 4];
+
+// // Destructuring of Array Leanthy method
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// console.log(a,b,c)
+
+// // Destructuring of Array ES6 method
+// const [x, y, z] = arr;
+// console.log(x,y,z)
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -11,6 +25,10 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +45,45 @@ const restaurant = {
     },
   },
 };
+
+
+// Destructuring of Arrays
+
+// Destructuring of Data 
+// const [first, second] = restaurant.categories
+// console.log(first, second)
+
+// Destructuring of Data and Skiping Data from Array
+// const [first, ,second] = restaurant.categories
+// console.log(first, second)
+
+
+let [main, ,secondary] = restaurant.categories;
+console.log(main, secondary)
+
+// Switching Variables Normal method
+// const temp = main
+// main = secondary
+// secondary = temp
+// console.log(main, secondary)
+
+// Switching Variables with data distructuring
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.order(2,0)
+console.log(starter, mainCourse)
+
+
+// Destructuring for nested Array
+
+const nested =  [2,4,[5,6]];
+// const [i,,j] = nested
+// console.log(i,j)
+
+const [i, ,[j,k]] = nested
+console.log(i,j,k)
+
+// Default Values
+const [p =1,q=1,r=1] = [8,9]
+console.log(p,q,r)
